e6ac36c4da5195e2036277fdbddf488caf4e1d5b
Veronika050987 <veronicaviv@mail.ru>
2025-05-18 21:07:37 +0300

    Add project files.

---
+++ Points.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    public class Points
+    {
+		public double X { get; set; }//Properties
+		public double Y { get; set; }//Properties
+
+		public Points(double x, double y)
+		{
+			Console.WriteLine($"Point constructor called for ({x}, {y})");
+			X = x;
+			Y = y;
+		}
+
+		public static double operator -(Points p1, Points p2)
+		{
+			double dx = p1.X - p2.X;
+			double dy = p1.Y - p2.Y;
+			return Math.Sqrt(dx * dx + dy * dy);//Math.sqrt() — это метод, который возвращает квадратный корень числа.
+												//Он является статическим методом объекта Math. 
+		}
+
+		public override string ToString()
+		{
+			return $"({X}, {Y})";
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Points p1 = new Points(1, 2);
+			Points p2 = new Points(4, 6);
+
+			double distance = p1 - p2;
+
+			Console.WriteLine($"Point 1: {p1}");
+			Console.WriteLine($"Point 2: {p2}");
+			Console.WriteLine($"Distance between p1 and p2: {distance}");
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			Console.WriteLine("Program finished!!!");
+			Console.ReadKey();
+		}
+    }
+}
